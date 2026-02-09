4cb959039787d7a1d0ea3c6145c4f3a18cbb3f0b
Łukasz Korecki <lukasz@korecki.me>
2026-02-09 13:42:38 -0800

    Update Homebrew formula for v0.0.3

--- HomebrewFormula/geppetto.rb
+++ HomebrewFormula/geppetto.rb
@@ -1,12 +1,12 @@
 class Geppetto < Formula
   desc "Lightweight process orchestrator for development environments"
   homepage "https://github.com/lukaszkorecki/geppetto"
-  version "0.0.2"
+  version "0.0.3"
 
   on_macos do
     on_arm do
       url "https://github.com/lukaszkorecki/geppetto/releases/download/v#{version}/geppetto-macos-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "a79580c465cc4ee3a0f9ea730ae0bac4107ddd3062464f16906a26e73184634b"
     end
   end
 
